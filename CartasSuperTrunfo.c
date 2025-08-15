895e46c95593e812dae50e4a7762e5b90928e363
vitorlimaofc <148359214+vitorlimaofc@users.noreply.github.com>
2025-08-15 01:26:58 +0000

    Desafio Aventureiro Concluido

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -12,7 +12,7 @@ int main() {
     char CodigodaCarta1[50], CodigodaCarta2[50]; 
     char cidade1[50], cidade2[50];
     int populacao1, populacao2, PontosTuristicos1, PontosTuristicos2;
-    float area1, area2, pib1, pib2; 
+    float area1, area2, pib1, pib2, densidadep1, densidadep2, PibperCapita1, PibperCapita2; 
    
 
     // Cadastro da 1° Carta. 
@@ -21,10 +21,10 @@ int main() {
     scanf("%c", &estado1); //Utiliza-se %c pois o estado é composto por somente 1 Caracter. 
 
     printf("Qual o código? \n");
-    scanf("%s", &CodigodaCarta1); // Utiliza-se %s (string) pois imprime uma cadeia de carateres. 
+    scanf("%s", CodigodaCarta1); // Utiliza-se %s (string) pois imprime uma cadeia de carateres. 
 
     printf("Qual o nome da cidade? \n");
-    scanf("%s", &cidade1);
+    scanf("%s", cidade1);
 
     printf("Qual a população? \n");
     scanf("%d", &populacao1); // Utiliza-se %d pois imprime um inteiro no formato decimal.
@@ -37,25 +37,32 @@ int main() {
 
     printf("Quantos pontos turísticos possui? \n");
     scanf("%d", &PontosTuristicos1);
+
+    densidadep1 = populacao1 / area1;
+    PibperCapita1 = pib1 / populacao1;
+
     //Depois de captar todos os dados irá imprimir os dados na tela do usuário. 
     printf("Estado: %c \n", estado1);
     printf("Código: %s \n", CodigodaCarta1);
     printf("Nome da Cidade: %s \n", cidade1);
     printf("População: %d \n", populacao1);
     printf("Área: %.2f km² \n", area1);
-    printf("PIB: %.2f \n", pib1);
+    printf("PIB: %.2f reais \n", pib1);
     printf("Número de Pontos Turísticos: %d \n", PontosTuristicos1);
+    printf("Densidade Populacional: %f hab/km² \n", densidadep1);
+    printf("Pib per Capta: %f reais \n", PibperCapita1);
+
 
     // Cadastro da 2° Carta. 
     printf("Vamos cadastrar a 2° Carta: \n");
     printf("Qual é o estado? \n");
-    scanf(" %c", &estado2);
+    scanf("%c", &estado2);
 
     printf("Qual o código? \n");
-    scanf("%s", &CodigodaCarta2);
+    scanf("%s", CodigodaCarta2);
 
     printf("Qual o nome da cidade? \n");
-    scanf("%s", &cidade2);
+    scanf("%s", cidade2);
 
     printf("Qual a população? \n");
     scanf("%d", &populacao2);
@@ -68,14 +75,19 @@ int main() {
 
     printf("Quantos pontos turísticos possui? \n");
     scanf("%d", &PontosTuristicos2);
+
+    densidadep2 = populacao2 / area2;
+    PibperCapita2 = pib2 / populacao2;
     
     printf("Estado: %c \n", estado2);
     printf("Código: %s \n", CodigodaCarta2);
     printf("Nome da Cidade: %s \n", cidade2);
     printf("População: %d \n", populacao2);
     printf("Área: %.2f km² \n", area2);
-    printf("PIB: %.2f \n", pib2);
+    printf("PIB: %.2f \n reais", pib2);
     printf("Número de Pontos Turísticos: %d \n", PontosTuristicos2);
+    printf("Densidade Populacional: %f hab/km² \n", densidadep2);
+    printf("Pib per Capta: %f reais \n", PibperCapita2);
 
 
     return 0;
